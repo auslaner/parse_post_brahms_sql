b417c7d02785e7824ceb55d9f99d080d69763ad8
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2021-10-20 17:37:22 -0600

    Fix for incorrect day range

--- parse.py
+++ parse.py
@@ -26,7 +26,7 @@ def process_bloom_time(bloom_time_string):
 
 def process_plant_date(day, month, year):
     try:
-        if (int(day) in range(1, 31)) and \
+        if (int(day) in range(1, 32)) and \
                 (int(month) in range(1, 13)) and \
                 (len(year) == 4):
 
